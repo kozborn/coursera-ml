973b91cf08e96e7e0ecde8f8fe4db4c6984c2872
Piotr Kozubek <piotrkozubek@gmail.com>
2018-11-20 16:46:38 +0100

    Update

--- iris-learning-nn/iris.m
+++ iris-learning-nn/iris.m
@@ -8,11 +8,29 @@ y = shuffledArray(:, end);
 
 
 labels_num = 3;
-hidden_layer_size = 5;
+hidden_layer_size = 2;
 lambda = 0;
 
 % adding bias unit
-a1 = [ones(size(X), 1), X]
-theta1 = rand(size(a1, 2), hidden_layer_size)
+a1 = [ones(size(X), 1), X];
+theta1 = rand(hidden_layer_size, size(a1, 2))
+
+z1 = a1*theta1';
+
+a2 = [ones(size(z1, 1), 1), sigmoid(z1)];
+theta2 = rand(labels_num, size(a2, 2));
+
+z2 = a2 * theta2';
+a3 = sigmoid(z2)
+
+
+theta1WithoutBias = theta1(:,2:end);
+theta2WithoutBias = theta2(:,2:end);
+sum1 = sum(sum(theta1WithoutBias.^2));
+sum2 = sum(sum(theta2WithoutBias.^2));
+
+
+% z2 = a2 * theta2';
+% a3 = sigmoid(z2);
 
 
